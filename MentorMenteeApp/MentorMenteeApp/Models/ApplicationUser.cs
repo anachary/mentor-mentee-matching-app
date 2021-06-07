883568d4debb94d2fa2a0d9c72b648de607e24df
Akash Acharya <akashnacharya@gmail.com>
2021-06-06 22:21:00 -0400

    initial setup react-app and server

---
+++ MentorMenteeApp/MentorMenteeApp/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentorMenteeApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        //public string GithubUrl { get; set; }
+
+        //public string LinkedinProfile { get; set; }
+
+        //public string PersonalWebsite { get; set; }
+
+        //public string PersonalDescription { get; set; }
+    }
+}
